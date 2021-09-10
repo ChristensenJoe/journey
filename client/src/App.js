0e12d9a7d8f4f70588549c82f0704f5bf1b4339e
Joe Christensen <joechristensenwork@gmail.com>
2021-09-10 11:53:36 -0700

    Added themeProvider

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,19 @@ import {
   useState,
   useEffect
 } from "react";
+
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom';
 
+import {
+  ThemeProvider
+} from '@material-ui/core'
+
 import './App.css';
+import theme from './Theme/theme.js'
 
 
 function App() {
@@ -22,18 +28,20 @@ function App() {
 
 
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/testing">
-            <h1>Test Route</h1>
-          </Route>
-          <Route path="/">
-            <h1>Page Count: {count}</h1>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/testing">
+              <h1>Test route</h1>
+            </Route>
+            <Route path="/">
+              <h1>Page Count: {count}</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ client/src/Theme/theme.js
@@ -0,0 +1,14 @@
+import {createTheme} from '@material-ui/core'
+
+const theme = createTheme({
+    typography: {
+        fontFamily: '"Inter", sans-serif'
+    },
+    palette: {
+        secondary: {
+            main: '#0000FF'
+        }
+    }
+})
+
+export default theme;
