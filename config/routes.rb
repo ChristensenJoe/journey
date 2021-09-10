cefa28b0b396744dfaca99b5afa30fcd2e983a5d
Joe Christensen <joechristensenwork@gmail.com>
2021-09-10 09:20:55 -0700

    Set up client-side routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-    namespace :api do
-    get '/hello', to: "application#hello_world"
-  end
+  get '/hello', to: "application#hello_world"
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
